6a018101c27f9e6be82fe53f97c0e28370f7afff
Master-Faraz <farazali9028@gmail.com>
2021-05-14 18:09:32 +0530

    ...

--- Code Chef/May Long Challange/Tik_Tak_Toe.cpp
+++ Code Chef/May Long Challange/Tik_Tak_Toe.cpp
@@ -1,29 +1,126 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int check(char a[3][3])
 {
-    char a[3][3];
 
+    int x = 0, o = 0;
+
+    if ((a[0][1] == a[0][2] == a[0][3] == (('X' || 'x'))) || (a[1][1] == a[1][2] == a[1][3] == (('X' || 'x'))) || (a[2][1] == a[2][2] == a[2][3] == (('X' || 'x'))))
+    {
+        x += 1;
+    }
+    if ((a[0][1] == a[0][2] == a[0][3] == (('O' || 'o'))) || (a[1][1] == a[1][2] == a[1][3] == (('O' || 'o'))) || (a[2][1] == a[2][2] == a[2][3] == (('O' || 'o'))))
+    {
+        o += 1;
+    }
+
+    if ((a[0][1] == a[1][1] == a[2][1] == (('X' || 'x'))) || (a[0][2] == a[1][2] == a[2][2] == (('X' || 'x'))) || (a[0][3] == a[1][3] == a[2][3] == (('X' || 'x'))))
+    {
+        x += 1;
+    }
+
+    if ((a[0][1] == a[1][1] == a[2][1] == (('O' || 'o'))) || (a[0][2] == a[1][2] == a[2][2] == (('O' || 'o'))) || (a[0][3] == a[1][3] == a[2][3] == (('O' || 'o'))))
+    {
+        o += 1;
+    }
+
+    if ((a[0][1] == a[1][2] == a[2][3] == (('X' || 'x'))) || (a[0][3] == a[1][2] == a[2][1] == (('X' || 'x'))))
+    {
+        x += 1;
+    }
+
+    if ((a[0][1] == a[1][2] == a[2][3] == (('O' || 'o'))) || (a[0][3] == a[1][2] == a[2][1] == (('O' || 'o'))))
+    {
+        o += 1;
+    }
+
+    if (((x > 0) && (o == 0)) || ((x = 0) && (o > 0)))
+        return 1;
+    else if (((x > 0) && (o > 0)))
+        return 3;
+    else
+        return 2;
+}
+
+int fill_X(char a[3][3])
+{
+    char temp[3][3];
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            cin >> a[i][j];
+            if (a[i][j] == '_')
+                temp[i][j] = 'X';
+            else
+                temp[i][j] = a[i][j];
         }
     }
 
-    if(( a[0][1] == a[0][2] == a[0][3] ) || ( a[1][1] == a[1][2] == a[1][3] ) || ( a[2][1] == a[2][2] == a[2][3] ))
-        cout<<"Winner  "<<endl;
+    if (check(temp) == 1)
+        return 1;
+    else if (check(temp) == 2)
+        return 2;
+    else if (check(temp) == 3)
+        return 3;
+}
 
-    else if(( a[0][1] == a[1][1] == a[2][1] ) || ( a[0][2] == a[1][2] == a[2][2] ) || ( a[0][3] == a[1][3] == a[2][3] ))
-        cout<<"Winner"<<endl;
+int fill_O(char a[3][3])
+{
+    char temp[3][3];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (a[i][j] == '_')
+                temp[i][j] = 'O';
+            else
+                temp[i][j] = a[i][j];
+        }
+    }
 
-    else if(( a[0][1] == a[1][2] == a[2][3] ) || ( a[0][3] == a[1][2] == a[2][1] ))
-        cout<<"Winner"<<endl;
+    if (check(temp) == 1)
+        return 1;
+    else if (check(temp) == 2)
+        return 2;
+    else if (check(temp) == 3)
+        return 3;
+}
 
-    else
-        cout<<"Looser"<<endl;
+int check_(char a[3][3])
+{
+    int nox = 0, noy = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (a[i][j] == 'X')
+                nox += 1;
+            else if (a[i][j] == 'O')
+                noy += 1;
+        }
+    }
+}
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+
+        char a[3][3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                cin >> a[i][j];
+            }
+        }
+    }
 
     return 0;
 }
@@ -36,3 +133,24 @@ int main()
 //     }
 //     cout<<endl;
 // }
+
+// if (a[i][j] == '_')
+// {
+//     if (fill_X(a) == 1)
+//         return 1;
+
+//     else if (fill_X(a) == 2)
+//         return 2;
+
+//     else
+//     {
+//         if (fill_O(a) == 1)
+//             return 1;
+
+//         else if (fill_O(a) == 2)
+//             return 2;
+
+//         else if (fill_O(a) == 3)
+//             return 3;
+
+//     }
--- pract.cpp
+++ pract.cpp
@@ -1,57 +1,162 @@
 #include <iostream>
 using namespace std;
 
-/* Iterative Function to calculate (x^y)%p in O(log y) */
-long long int power(long long x, unsigned int y, int p)
-{
-    long long int res = 1;
-
-    x = x % p;
-
-    if (x == 0)
-        return 0;
-
-    while (y > 0)
-    {
-        if (y & 1)
-            res = (res * x) % p;
-
-        y = y >> 1;
-        x = (x * x) % p;
-    }
-    return res;
-}
-
-long long int power(long long int x, long long int y)
-{
-    long long int res = 1;
-
-    while (y)
-    {
-        if (y % 2 == 1)
-            res = (res * x);
-
-        y = y >> 1;
-
-        x = (x * x);
-    }
-    return res;
-}
-
-
 int main()
 {
-    long long int x = 2;
-    long long int y, t;
-    long long int p = 1000000007;
+    int t;
     cin >> t;
+
     while (t--)
     {
-        cin >> y;
-        y = y - 1;
-        cout<<power(x, y, p)<<endl;        
+        int X_win = 0, O_win = 0, X_count = 0, O_count = 0, underscore_count = 0;
+        char a[3][3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                cin >> a[i][j];
+                if (a[i][j] == 'X')
+                    X_count++;
+                else if (a[i][j] == 'O')
+                    O_count++;
+                else
+                    underscore_count++;
+            }
+        }
+
+        //.                     *****       Checking winning condition      *****
+
+        //.     Rows
+        if (((a[0][0] == a[0][1]) and (a[0][1] == a[0][2]) and (a[0][1] == 'X')) || ((a[1][0] == a[1][1]) and (a[1][1] == a[1][2]) and (a[1][1] == 'X')) || ((a[2][0] == a[2][1]) and (a[2][1] == a[2][2]) and (a[2][1] == 'X'))) // .    X - Rows
+        {
+            X_win++;
+        }
+
+        if (((a[0][0] == a[0][1]) and (a[0][1] == a[0][2]) and (a[0][1] == 'O')) || ((a[1][0] == a[1][1]) and (a[1][1] == a[1][2]) and (a[1][1] == 'O')) || ((a[2][0] == a[2][1]) and (a[2][1] == a[2][2]) and (a[2][1] == 'O'))) //.              O - Rows
+        {
+            O_win++;
+        }
+
+        //.             Columns
+
+        if (((a[0][0] == a[1][0]) and (a[1][0] == a[2][0]) and (a[0][0] == 'X')) || ((a[0][1] == a[1][1]) and (a[1][1] == a[2][1]) and (a[2][1] == 'X')) || ((a[0][2] == a[1][2]) and (a[1][2] == a[2][2]) and (a[2][2] == 'X'))) // .    X - column
+        {
+            X_win++;
+        }
+
+        if (((a[0][0] == a[1][0]) and (a[1][0] == a[2][0]) and (a[0][0] == 'O')) || ((a[0][1] == a[1][1]) and (a[1][1] == a[2][1]) and (a[2][1] == 'O')) || ((a[0][2] == a[1][2]) and (a[1][2] == a[2][2]) and (a[2][2] == 'O'))) //.              O - column
+        {
+            O_win++;
+        }
+
+        //.         Diagonal
+
+        if (((a[0][0] == a[1][1]) and (a[1][1] == a[2][2])))
+        {
+            if (a[0][0] == 'X')
+                X_win++;
+            else
+                O_win++;
+        }
+        else if (((a[0][2] == a[1][1]) and (a[1][1] == a[2][0])))
+        {
+            if (a[1][1] == 'X')
+                X_win++;
+            else
+                O_win++;
+        }
+
+        // //.                 ***** checking and printing *****
+
+        if (O_count > X_count) //.                 number of  O > X
+            cout << 3 << endl;
+        else if ((X_count == 1 and O_count == 0) and (underscore_count = 8))
+            cout << 2 << endl;
+        else if ((X_count == 0 and O_count == 1) and (underscore_count = 8))
+            cout << 2 << endl;
+
+        else if ((X_count == 0 and O_count == 0) and (underscore_count = 9))
+            cout << 2 << endl;
+
+        else if ((X_count - O_count) > 1) //.         more X than O
+            cout << 3 << endl;
+
+        else if (((X_count > O_count) and ((X_win == 1) and (O_win == 0)))) //.   last value put by X and it wins
+            cout << 1 << endl;
+
+        else if ((X_count == O_count) and ((X_win == 0) and (O_win == 1))) //.  last Value put by O and it wins
+            cout << 1 << endl;
+
+        else if (((X_win + O_win) == 0) and (underscore_count == 0)) //.     Draw Condition
+            cout << 1 << endl;
+
+        else if ((((X_win == 2) and (underscore_count == 0)))) //.     Special case in which X wins by rows and digonal full
+            cout << 1 << endl;
+
+        else if ((underscore_count > 0) and ((X_win + O_win == 0))) //.     when game is not finished
+            cout << 2 << endl;
+
+        else
+            cout << 3 << endl;
     }
-    
+
     return 0;
 }
 
+// cout << "\n\n X Count : " << X_count << " O count : " << O_count << " UnderScore count : " << underscore_count << endl;
+// cout << X_win << "\t" << O_win << endl;
+
+// for (int i = 0; i < 3; i++)
+// {
+//     for (int j = 0; j < 3; j++)
+
+//     {
+//         cout<< a[i][j]<<"\t";
+//     }
+//     cout<<endl;
+// }
+// cout<<"\nX count = "<<X_count<<" O count = "<<O_count<<" Underscore "<<underscore_count<<endl;
+
+/*
+
+                                TEST CASES
+1
+X O X
+X O X
+0 X X
+
+1
+X O X
+X O X
+O O O  
+
+1
+X O X
+X O X
+X O O  
+
+1
+X X O
+X O X
+X O O
+
+1
+O O X
+O X O
+O X X
+
+O X X 
+O _ X
+O _ _
+
+1
+X O O
+X _ _
+X _ _
+
+XOX
+XXO
+OXO
+
+*/
