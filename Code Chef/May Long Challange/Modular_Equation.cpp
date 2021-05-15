18e409bf02d9a6a262e05be255a05034db57c127
Master-Faraz <farazali9028@gmail.com>
2021-05-15 23:10:59 +0530

    modular done

--- Code Chef/May Long Challange/Modular_Equation.cpp
+++ Code Chef/May Long Challange/Modular_Equation.cpp
@@ -1,21 +1,54 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int n = 0, m = 0, x = 1, y = 1, a = 1, b = 2, count = 0;
+//         cin >> n >> m;
+
+//         for (a = 1; a <= n - 1; a++)
+//         {
+//             for (b = a + 1; b <= n; b++)
+//             {
+//                 x = ((m % a) % b);
+//                 y = ((m % b) % a);
+//                 if ((a != b) && (x == y))
+//                 {
+//                     // cout << a << "\t" << b << "\t" << x << "\t" << y << endl;
+//                     count++;
+//                 }
+//             }
+//         }
+//         cout << count << endl;
+//     }
+//     return 0;
+// }
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    long long int n, m, x = 1, y = 1, a = 1, b = 3;
-    cin >> n >> m;
-
-    x = ((m % a) % b);
-    y = ((m % b) % a);
-
-    // for (int i; i < n - 1; i++)
-    // {
-    //     cout << a << "\t" << b << "\t" <<x << "\t" << y << endl;
-    //     a = b;
-    //     b = b + 1;
-    // }
-    cout<<x<<"\t"<<y<<endl;
-
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int n = 0, m = 0, count = 0;
+        cin >> n >> m;
+        vector<long long int> v(n + 1, 1);
+        for (long int i = 2; i <= n; i++)
+        {
+            long long int a = (m % i);
+            count += v[a];
+            for(long int j=a;j<=n;j+=i)
+            {
+                v[j]++;
+            }
+        }
+        cout << count << endl;
+    }
     return 0;
 }
