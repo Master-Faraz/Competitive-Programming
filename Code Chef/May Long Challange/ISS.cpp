d5b681a29a229b511807929fde10056297507413
Master-Faraz <farazali9028@gmail.com>
2021-05-17 04:23:45 +0530

    done

--- Code Chef/May Long Challange/ISS.cpp
+++ Code Chef/May Long Challange/ISS.cpp
@@ -2,10 +2,14 @@
 #include <vector>
 using namespace std;
 
-long long int gcd(long long int a, long long int b);
+// long long int gcd(long long int a, long long int b);
 long long int power(long long int x, long long int y);
-
-
+long long int gcd(long long int a, long long int b)
+{
+    while (b)
+        b ^= a ^= b ^= a %= b;
+    return a;
+}
 
 int main()
 {
@@ -13,29 +17,44 @@ int main()
     cin >> t;
     while (t--)
     {
-        long long int k;
-        cin >> k;
-        for (int i = 1; i < (2 * k + 1); i++)
-        {
-           vector<long long int> v[k];
-                      
+        long long int k, seq, sum = 0;
 
+        cin >> k;
+
+        vector<long long int> v[1];
+
+        for (int i = 1; i <= (2 * k + 1); i++)
+        {
+
+            seq = (k + power(i, 2));
+            v->push_back(seq);
         }
+
+        vector<long long int>::iterator it1;
+
+        for (auto itr = v->begin(); itr != v->end(); itr++)
+        {
+            itr++;
+            it1 = itr;
+            itr--;
+            if (*it1 != 0)
+                sum += gcd(*itr, *it1);
+            // cout<<*itr<<endl;
+            // cout<<*itr<<"\t"<<*it1<<endl;
+        }
+
+        cout << sum << endl;
     }
+
     return 0;
 }
 
-long long int gcd(long long int a, long long int b)
-{
-    while (a != b)
-    {
-        if (a > b)
-            a -= b;
-        else
-            b -= a;
-    }
-    return a;
-}
+//.                                   ****************************
+
+// for (auto itr = v->begin(); itr != v->end(); itr++)
+//     cout << *itr << endl;
+
+//.                                   ****************************
 
 long long int power(long long int x, long long int y)
 {
