b76410cb1b21bf9e4d6bc4413672a04f4c143146
Master-Faraz <farazali9028@gmail.com>
2022-01-30 01:00:51 +0530

    codeforces updated

--- pract.cpp
+++ pract.cpp
@@ -38,32 +38,13 @@ int main()
       cout << 2 << endl;
     else
     {
-      int val = (h / (largest + second_Largest)) * 2 + 1;
-      cout << val << endl;
+      int val = (h / (largest + second_Largest));
+      int res = h - (largest + second_Largest) * val;
+      if ((res - largest <= 0) || (res - second_Largest <= 0))
+        cout << val * 2 + 1 << endl;
+      else
+        cout << val * 2 + 2;
     }
   }
   return 0;
 }
-
-// else
-// {
-//   long long int count = 2, sum = largest + second_Largest;
-//   int flip_flop = 0;
-
-//   while (sum < h)
-//   {
-//     if (flip_flop == 0)
-//     {
-//       sum += largest;
-//       flip_flop++;
-//       count++;
-//     }
-//     else
-//     {
-//       sum += second_Largest;
-//       flip_flop--;
-//       count++;
-//     }
-//   }
-//   cout << count << endl;
-// }
